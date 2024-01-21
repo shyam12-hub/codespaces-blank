9eda00caf308e045b397cfa142c3ed640a75e5c0
shyam12-hub <85274492+shyam12-hub@users.noreply.github.com>
2024-01-21 05:43:53 +0000

    new changes occur

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,24 @@
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";;
 import { useEffect } from "react";
 import { fetchProduct } from "./feature/ProductSlice";
-
-function App() {
-  const dispatch = useDispatch();
-  const loading = useSelector((state) => state.isLoading);
-  const products = useSelector((state) => state.products);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        await dispatch(fetchProduct());
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
-    fetchData();
-  }, [dispatch]);
-
-  return (
-    <div>
-      <h1>{loading ? "Loading...." : "Hello ji"}</h1>
-      
-      {products && (
-        <ul>
-          {products.map((product) => (
-            <li key={product.id}>{product.name}</li>
-          ))}
-        </ul>
-      )}
-    </div>
+function App(){
+  const dispatch = useDispatch()
+  const products = useSelector(state => state.products);
+  const loading = useSelector(state => state.isLoading)
+  useEffect(()=>{
+const fetchData = async ()=>{
+  try{
+    dispatch(fetchProduct());
+console.log(products)
+  }
+  catch(error){
+    console.log(error)
+  }
+}
+fetchData()
+  },[])
+  return(
+<div>{loading ? "loading...": "hello ji"}</div>
   );
 }
-
 export default App;
--- src/feature/ProductSlice.js
+++ src/feature/ProductSlice.js
@@ -6,28 +6,15 @@ const initialState = {
   isLoading: false, // Corrected typo here
 };
 
-const options = {
-  method: "GET",
-  url: "https://real-time-product-search.p.rapidapi.com/search",
-  params: {
-    q: "Nike shoes",
-    country: "us",
-    language: "en",
-  },
-  headers: {
-    "X-RapidAPI-Key": "db39858a17msh01e5f95254e7825p1051cajsnb2d8261f8cd2",
-    "X-RapidAPI-Host": "real-time-product-search.p.rapidapi.com",
-  },
-};
-
-export const fetchProduct = createAsyncThunk("fetchProduct", async () => {
-  try {
-    const response = await axios.request(options);
-    return response.data;
-  } catch (error) {
-    console.log(error);
+export const fetchProduct = createAsyncThunk("fetchProduct", async()=>{
+  try{
+    const response = await axios.request("https://dummyjson.com/");
+    return response.data
   }
-});
+  catch(error){
+    console.log(error)
+  }
+})
 
 export const ProductSlice = createSlice({
   name: "product",
@@ -39,7 +26,10 @@ export const ProductSlice = createSlice({
     });
 
     // api call done
-    builder.addCase(fetchProduct.fulfilled, (state, action) => (state.isLoading= false,state.products= action.payload) );
+    builder.addCase(fetchProduct.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.products = action.payload;
+    });
 
     // in error
     builder.addCase(fetchProduct.rejected, () => {
